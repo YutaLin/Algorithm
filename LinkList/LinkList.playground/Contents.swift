383c56c170b2b17e6f1d713b3546bbbd5a6a1915
Yuta Lin <linyuta1230@gmail.com>
2019-12-31 09:54:41 +0800

    add node and example

--- LinkList/LinkList.playground/Contents.swift
+++ LinkList/LinkList.playground/Contents.swift
@@ -1,3 +1,41 @@
 import UIKit
 
-var str = "Hello, playground"
+func example(of example: String, implementation: () -> ()) {
+    print(example)
+    implementation()
+}
+
+public class Node<Value> {
+    
+    public var value: Value
+    public var next: Node?
+    
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node: CustomStringConvertible {
+    
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
+example(of: "creating and linking nodes") {
+    
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+    
+    node1.next = node2
+    node2.next = node3
+    
+    print(node1)
+}
+
